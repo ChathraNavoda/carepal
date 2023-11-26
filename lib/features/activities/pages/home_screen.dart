4b99d44c2eed089533e994a0c69466ba1eb28def
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-11-26 16:59:08 +0530

    app const colors

--- lib/features/activities/pages/home_screen.dart
+++ lib/features/activities/pages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carepal/common/utils/constants.dart';
 import 'package:carepal/common/widgets/appStyle.dart';
 import 'package:carepal/common/widgets/reusable_text.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +15,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: ReusableText(
-              text: "CarePal Health Tracker",
-              style: appStyle(
-                  25, const Color.fromARGB(255, 45, 200, 227), FontWeight.bold))),
+        child: ReusableText(
+          text: "CarePal Health Tracker",
+          style: appStyle(25, AppConst.kBlueLight, FontWeight.bold),
+        ),
+      ),
     );
   }
 }
