9400dd5f7f807807d7f5f75714c15e1c210432cc
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-11-27 08:58:59 +0530

    customize spacer

--- lib/features/activities/pages/home_screen.dart
+++ lib/features/activities/pages/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carepal/common/utils/constants.dart';
 import 'package:carepal/common/widgets/appStyle.dart';
+import 'package:carepal/common/widgets/height_spacer.dart';
 import 'package:carepal/common/widgets/reusable_text.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +15,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ReusableText(
-          text: "CarePal Health Tracker",
-          style: appStyle(25, AppConst.kBlueLight, FontWeight.bold),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ReusableText(
+            text: "CarePal Health Tracker",
+            style: appStyle(25, AppConst.kBlueLight, FontWeight.bold),
+          ),
+          const HeightSpacer(
+            height: 50,
+          ),
+          ReusableText(
+            text: "CarePal Health Tracker",
+            style: appStyle(25, AppConst.kBlueLight, FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
