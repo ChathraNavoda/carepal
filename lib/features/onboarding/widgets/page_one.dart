f9d700651d2264e010562d153a8e0bc1ca13b878
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-11-27 12:04:22 +0530

    onboard page one

--- lib/features/onboarding/widgets/page_one.dart
+++ lib/features/onboarding/widgets/page_one.dart
@@ -1,4 +1,7 @@
 import 'package:carepal/common/utils/constants.dart';
+import 'package:carepal/common/widgets/appStyle.dart';
+import 'package:carepal/common/widgets/height_spacer.dart';
+import 'package:carepal/common/widgets/reusable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -19,6 +22,29 @@ class PageOne extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
             child: Image.asset("assets/images/activities.png"),
           ),
+          const HeightSpacer(height: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ReusableText(
+                text: "Track My Activities",
+                style: appStyle(
+                  30,
+                  AppConst.kLight,
+                  FontWeight.w600,
+                ),
+              ),
+              const HeightSpacer(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.w),
+                child: Text(
+                  "Welcome to CarePal!",
+                  textAlign: TextAlign.center,
+                  style: appStyle(16, AppConst.kGreyLight, FontWeight.normal),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
