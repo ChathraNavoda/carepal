f1e6e66de23258ad220a2d5aa95e0a651bc22577
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-11-27 15:28:09 +0530

    custom text field

--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -16,4 +16,5 @@ class AppConst {
 
   static double kWidth = 375.w;
   static double kHeight = 825.h;
+  static double kRadius = 12.h;
 }
---
+++ lib/common/widgets/custom_text.dart
@@ -0,0 +1,70 @@
+import 'package:carepal/common/utils/constants.dart';
+import 'package:carepal/common/widgets/appStyle.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    this.keyboardType,
+    required this.hintText,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.hintStyle,
+    required this.controller,
+    this.onChanged,
+  });
+
+  final TextInputType? keyboardType;
+  final String hintText;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final TextStyle? hintStyle;
+  final TextEditingController controller;
+  final void Function(String)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: AppConst.kWidth * 0.9,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(AppConst.kRadius),
+        ),
+      ),
+      child: TextFormField(
+        keyboardType: keyboardType,
+        controller: controller,
+        cursorHeight: 25,
+        onChanged: onChanged,
+        style: appStyle(18, AppConst.kBkDark, FontWeight.w700),
+        decoration: InputDecoration(
+          hintText: hintText,
+          suffixIcon: suffixIcon,
+          prefixIcon: prefixIcon,
+          suffixIconColor: AppConst.kBkDark,
+          hintStyle: hintStyle,
+          errorBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppConst.kRed, width: 0.5),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: Colors.transparent, width: 0.5),
+          ),
+          focusedErrorBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppConst.kRed, width: 0.5),
+          ),
+          disabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppConst.kGreyDk, width: 0.5),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.zero,
+            borderSide: BorderSide(color: AppConst.kBkDark, width: 0.5),
+          ),
+        ),
+      ),
+    );
+  }
+}
